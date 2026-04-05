d6c24b682f0013be245a8c756a59dde01902fca9
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-05 12:09:18 +0700

    Create Home.java

---
+++ src/main/java/me/aris/core/models/Home.java
@@ -0,0 +1,17 @@
+package me.aris.core.models;
+
+import org.bukkit.Location;
+
+public class Home {
+    private String name;
+    private Location location;
+    
+    public Home(String name, Location location) {
+        this.name = name;
+        this.location = location;
+    }
+    
+    public String getName() { return name; }
+    public Location getLocation() { return location; }
+    public void setLocation(Location location) { this.location = location; }
+}

cbe13781670528f443f813b4e681d326aa5e483f
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-05 11:47:48 +0700

    Create HomeCommand.java

---
+++ src/main/java/me/aris/core/commands/home/HomeCommand.java
@@ -0,0 +1,51 @@
+package me.aris.core.commands.home;
+
+import me.aris.core.ArisCore;
+import me.aris.core.models.Home;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HomeCommand implements CommandExecutor {
+    private ArisCore plugin;
+    
+    public HomeCommand(ArisCore plugin) {
+        this.plugin = plugin;
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            plugin.getMessageManager().sendMessage(null, "player-only", "home");
+            return true;
+        }
+        
+        Player player = (Player) sender;
+        
+        if (!player.hasPermission("ariscore.home")) {
+            plugin.getMessageManager().sendMessage(player, "no-permission", "home");
+            return true;
+        }
+        
+        String homeName = args.length > 0 ? args[0] : "home";
+        
+        if (!plugin.getHomeManager().hasHome(player, homeName)) {
+            plugin.getMessageManager().sendMessage(player, "home-not-found", "home", "home", homeName);
+            return true;
+        }
+        
+        Home home = plugin.getHomeManager().getHome(player, homeName);
+        
+        plugin.getTeleportManager().startTeleport(player, home.getLocation(),
+            () -> {
+                plugin.getMessageManager().sendMessage(player, "teleport-success", "home", "home", homeName);
+            },
+            () -> {
+                plugin.getMessageManager().sendMessage(player, "teleport-cancelled-movement", "home");
+            }
+        );
+        
+        return true;
+    }
+          }

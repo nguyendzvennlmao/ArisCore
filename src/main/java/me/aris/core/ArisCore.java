fb6643b74da58c32a6ff1fe39af55a277045313d
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-05 16:06:06 +0700

    Update ArisCore.java

--- src/main/java/me/aris/core/ArisCore.java
+++ src/main/java/me/aris/core/ArisCore.java
@@ -24,9 +24,13 @@ import me.aris.core.managers.*;
 import me.aris.core.teleport.TeleportExecutor;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class ArisCore extends JavaPlugin {
     private static ArisCore instance;
@@ -50,9 +54,7 @@ public class ArisCore extends JavaPlugin {
         
         printLogo();
         
-        saveDefaultConfig();
-        
-        createDataFolders();
+        createAllConfigs();
         
         configManager = new ConfigManager(this);
         messageManager = new MessageManager(this);
@@ -89,8 +91,9 @@ public class ArisCore extends JavaPlugin {
         }
     }
     
-    private void createDataFolders() {
+    private void createAllConfigs() {
         createFolder("");
+        createFolder("Location");
         createFolder("Afk");
         createFolder("Home");
         createFolder("Home/gui");
@@ -100,10 +103,30 @@ public class ArisCore extends JavaPlugin {
         createFolder("Warp");
         createFolder("Rtp");
         
-        createFile("spawn.yml");
-        createFile("homes.yml");
-        createFile("warps.yml");
-        createFile("afk-location.yml");
+        createConfig("config.yml", "");
+        createConfig("Afk/config.yml", "Afk/config.yml");
+        createConfig("Afk/message.yml", "Afk/message.yml");
+        createConfig("Home/config.yml", "Home/config.yml");
+        createConfig("Home/message.yml", "Home/message.yml");
+        createConfig("Home/gui/home.yml", "Home/gui/home.yml");
+        createConfig("Home/gui/confirm.yml", "Home/gui/confirm.yml");
+        createConfig("Spawn/config.yml", "Spawn/config.yml");
+        createConfig("Spawn/message.yml", "Spawn/message.yml");
+        createConfig("Tpa/config.yml", "Tpa/config.yml");
+        createConfig("Tpa/message.yml", "Tpa/message.yml");
+        createConfig("Tpa/gui/tpa.yml", "Tpa/gui/tpa.yml");
+        createConfig("Tpa/gui/tpahere.yml", "Tpa/gui/tpahere.yml");
+        createConfig("Warp/config.yml", "Warp/config.yml");
+        createConfig("Warp/message.yml", "Warp/message.yml");
+        createConfig("Warp/gui.yml", "Warp/gui.yml");
+        createConfig("Rtp/config.yml", "Rtp/config.yml");
+        createConfig("Rtp/message.yml", "Rtp/message.yml");
+        createConfig("Rtp/gui.yml", "Rtp/gui.yml");
+        
+        createLocationFile("spawn.yml");
+        createLocationFile("afk.yml");
+        createLocationFile("warp.yml");
+        createLocationFile("home.yml");
     }
     
     private void createFolder(String path) {
@@ -113,13 +136,31 @@ public class ArisCore extends JavaPlugin {
         }
     }
     
-    private void createFile(String path) {
-        File file = new File(getDataFolder(), path);
+    private void createConfig(String targetPath, String sourcePath) {
+        File file = new File(getDataFolder(), targetPath);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+                if (sourcePath != null && !sourcePath.isEmpty()) {
+                    InputStream is = getResource(sourcePath);
+                    if (is != null) {
+                        YamlConfiguration yaml = YamlConfiguration.loadConfiguration(new InputStreamReader(is, StandardCharsets.UTF_8));
+                        yaml.save(file);
+                    }
+                }
+            } catch (IOException e) {
+                getLogger().warning("Failed to create " + targetPath + ": " + e.getMessage());
+            }
+        }
+    }
+    
+    private void createLocationFile(String fileName) {
+        File file = new File(getDataFolder(), "Location/" + fileName);
         if (!file.exists()) {
             try {
                 file.createNewFile();
             } catch (IOException e) {
-                getLogger().warning("Failed to create file: " + path);
+                getLogger().warning("Failed to create Location/" + fileName + ": " + e.getMessage());
             }
         }
     }
@@ -134,7 +175,7 @@ public class ArisCore extends JavaPlugin {
     }
 
     private void registerCommands() {
-        if (configManager.isModuleEnabled("tpa")) {
+        if (configManager != null && configManager.isModuleEnabled("tpa")) {
             getCommand("tpa").setExecutor(new TPACommand(this));
             getCommand("tpahere").setExecutor(new TPAHereCommand(this));
             getCommand("tpaccept").setExecutor(new TPAcceptCommand(this));
@@ -145,44 +186,44 @@ public class ArisCore extends JavaPlugin {
             getCommand("tpauto").setExecutor(new TPAutoCommand(this));
         }
         
-        if (configManager.isModuleEnabled("home")) {
+        if (configManager != null && configManager.isModuleEnabled("home")) {
             getCommand("home").setExecutor(new HomeCommand(this));
             getCommand("sethome").setExecutor(new SetHomeCommand(this));
             getCommand("delhome").setExecutor(new DelHomeCommand(this));
         }
         
-        if (configManager.isModuleEnabled("warp")) {
+        if (configManager != null && configManager.isModuleEnabled("warp")) {
             getCommand("warp").setExecutor(new WarpCommand(this));
             getCommand("setwarp").setExecutor(new SetWarpCommand(this));
             getCommand("delwarp").setExecutor(new DelWarpCommand(this));
         }
         
-        if (configManager.isModuleEnabled("spawn")) {
+        if (configManager != null && configManager.isModuleEnabled("spawn")) {
             getCommand("spawn").setExecutor(new SpawnCommand(this));
             getCommand("setspawn").setExecutor(new SetSpawnCommand(this));
             getCommand("delspawn").setExecutor(new DelSpawnCommand(this));
         }
         
-        if (configManager.isModuleEnabled("afk")) {
+        if (configManager != null && configManager.isModuleEnabled("afk")) {
             getCommand("afk").setExecutor(new AFKCommand(this));
             getCommand("setafk").setExecutor(new SetAFKCommand(this));
             getCommand("delafk").setExecutor(new DelAFKCommand(this));
         }
         
-        if (configManager.isModuleEnabled("rtp")) {
+        if (configManager != null && configManager.isModuleEnabled("rtp")) {
             getCommand("rtp").setExecutor(new RTPCommand(this));
         }
     }
 
     private void registerListeners() {
         getServer().getPluginManager().registerEvents(new TeleportListener(this), this);
-        if (configManager.isModuleEnabled("afk")) {
+        if (configManager != null && configManager.isModuleEnabled("afk")) {
             getServer().getPluginManager().registerEvents(new AFKListener(this), this);
         }
         getServer().getPluginManager().registerEvents(homeGUI, this);
         getServer().getPluginManager().registerEvents(confirmGUI, this);
         getServer().getPluginManager().registerEvents(warpGUI, this);
-        if (configManager.isModuleEnabled("rtp")) {
+        if (configManager != null && configManager.isModuleEnabled("rtp")) {
             getServer().getPluginManager().registerEvents(rtpGUI, this);
         }
     }
@@ -201,4 +242,4 @@ public class ArisCore extends JavaPlugin {
     public WarpGUI getWarpGUI() { return warpGUI; }
     public ConfirmGUI getConfirmGUI() { return confirmGUI; }
     public RTPGUI getRTPGUI() { return rtpGUI; }
-    }
+            }

1ff200419ab78644a7ed5f7873b4cedd7750e6aa
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-06 14:54:41 +0700

    Update PlaceholderHook.java

--- src/main/java/me/aris/core/hooks/PlaceholderHook.java
+++ src/main/java/me/aris/core/hooks/PlaceholderHook.java
@@ -1,10 +1,9 @@
 package me.aris.core.hooks;
 
 import me.aris.core.ArisCore;
-import me.aris.core.managers.ShardsManager;
 import org.bukkit.entity.Player;
 
-public class PlaceholderHook implements me.clip.placeholderapi.expansion.PlaceholderExpansion {
+public class PlaceholderHook {
     
     private ArisCore plugin;
     
@@ -12,48 +11,58 @@ public class PlaceholderHook implements me.clip.placeholderapi.expansion.Placeho
         this.plugin = plugin;
     }
     
-    @Override
-    public String getIdentifier() {
-        return "arisshards";
+    public void register() {
+        if (plugin.getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new PlaceholderExpansion().register();
+            plugin.getLogger().info("PlaceholderAPI expansion registered!");
+        }
     }
     
-    @Override
-    public String getAuthor() {
-        return "ArisCore";
-    }
-    
-    @Override
-    public String getVersion() {
-        return "1.0";
-    }
-    
-    @Override
-    public boolean persist() {
-        return true;
-    }
-    
-    @Override
-    public String onPlaceholderRequest(Player player, String identifier) {
-        if (player == null) {
-            return "";
+    private class PlaceholderExpansion extends me.clip.placeholderapi.expansion.PlaceholderExpansion {
+        
+        @Override
+        public String getIdentifier() {
+            return "arisshards";
         }
         
-        if (identifier.equals("balance")) {
-            long balance = plugin.getShardsManager().getBalance(player);
-            return formatNumber(balance);
+        @Override
+        public String getAuthor() {
+            return "ArisCore";
         }
         
-        return null;
-    }
-    
-    private String formatNumber(long number) {
-        if (number >= 1000000000) {
-            return String.format("%.1f", number / 1000000000.0) + "B";
-        } else if (number >= 1000000) {
-            return String.format("%.1f", number / 1000000.0) + "M";
-        } else if (number >= 1000) {
-            return String.format("%.1f", number / 1000.0) + "K";
+        @Override
+        public String getVersion() {
+            return "1.0";
         }
-        return String.valueOf(number);
+        
+        @Override
+        public boolean persist() {
+            return true;
+        }
+        
+        @Override
+        public String onPlaceholderRequest(Player player, String identifier) {
+            if (player == null) {
+                return "";
+            }
+            
+            if (identifier.equals("balance")) {
+                long balance = plugin.getShardsManager().getBalance(player);
+                return formatNumber(balance);
+            }
+            
+            return null;
+        }
+        
+        private String formatNumber(long number) {
+            if (number >= 1000000000) {
+                return String.format("%.1f", number / 1000000000.0) + "B";
+            } else if (number >= 1000000) {
+                return String.format("%.1f", number / 1000000.0) + "M";
+            } else if (number >= 1000) {
+                return String.format("%.1f", number / 1000.0) + "K";
+            }
+            return String.valueOf(number);
+        }
     }
-          }
+        }

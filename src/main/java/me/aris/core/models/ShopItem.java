f2ee84fd77ec06e7fb950ba8b87fe0f8c354684f
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-07 08:59:57 +0700

    Update ShopItem.java

--- src/main/java/me/aris/core/models/ShopItem.java
+++ src/main/java/me/aris/core/models/ShopItem.java
@@ -8,8 +8,9 @@ public class ShopItem {
     private String displayName;
     private String command;
     private int amount;
+    private String currency;
     
-    public ShopItem(String category, String itemKey, long price, String materialName, String displayName, String command, int amount) {
+    public ShopItem(String category, String itemKey, long price, String materialName, String displayName, String command, int amount, String currency) {
         this.category = category;
         this.itemKey = itemKey;
         this.price = price;
@@ -17,6 +18,7 @@ public class ShopItem {
         this.displayName = displayName;
         this.command = command;
         this.amount = amount;
+        this.currency = currency;
     }
     
     public String getCategory() { return category; }
@@ -26,5 +28,6 @@ public class ShopItem {
     public String getDisplayName() { return displayName; }
     public String getCommand() { return command; }
     public int getAmount() { return amount; }
+    public String getCurrency() { return currency; }
     public void setAmount(int amount) { this.amount = amount; }
-}
+    }

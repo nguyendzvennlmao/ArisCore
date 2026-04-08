eb74d4162e18ac6015a913d074ac86acc72c9dc7
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-08 18:58:26 +0700

    Update TPAMessageManager.java

--- src/main/java/me/aris/core/tpa/message/TPAMessageManager.java
+++ src/main/java/me/aris/core/tpa/message/TPAMessageManager.java
@@ -96,4 +96,4 @@ public class TPAMessageManager {
         placeholders.put(placeholder, value);
         sendMessage(player, path, placeholders);
     }
-  }
+                                                 }

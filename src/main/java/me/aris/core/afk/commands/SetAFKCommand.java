e35ff8a97f485a9834e9fa5d1f3cb407dca08cc5
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-08 16:51:25 +0700

    Create SetAFKCommand.java

---
+++ src/main/java/me/aris/core/afk/commands/SetAFKCommand.java
@@ -0,0 +1,35 @@
+package me.aris.core.afk.commands;
+
+import me.aris.core.ArisCore;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SetAFKCommand implements CommandExecutor {
+    private ArisCore plugin;
+    
+    public SetAFKCommand(ArisCore plugin) {
+        this.plugin = plugin;
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            plugin.getAFKMessageManager().sendMessage(null, "player-only");
+            return true;
+        }
+        
+        Player player = (Player) sender;
+        
+        if (!player.hasPermission("ariscore.setafk")) {
+            plugin.getAFKMessageManager().sendMessage(player, "no-permission");
+            return true;
+        }
+        
+        plugin.getAFKManager().setAFKLocation(player.getLocation());
+        plugin.getAFKMessageManager().sendMessage(player, "afk-location-set");
+        
+        return true;
+    }
+}

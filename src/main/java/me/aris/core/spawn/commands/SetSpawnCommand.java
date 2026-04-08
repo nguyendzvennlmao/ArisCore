dc64dfbac45a7d10cc3fb19011949d16d278c3b7
nguyendzvennlmao <tqnmods111@gmail.com>
2026-04-08 16:45:06 +0700

    Create SetSpawnCommand.java

---
+++ src/main/java/me/aris/core/spawn/commands/SetSpawnCommand.java
@@ -0,0 +1,36 @@
+package me.aris.core.spawn.commands;
+
+import me.aris.core.ArisCore;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SetSpawnCommand implements CommandExecutor {
+    private ArisCore plugin;
+    
+    public SetSpawnCommand(ArisCore plugin) {
+        this.plugin = plugin;
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            plugin.getSpawnMessageManager().sendMessage(null, "player-only");
+            return true;
+        }
+        
+        Player player = (Player) sender;
+        
+        if (!player.hasPermission("ariscore.setspawn")) {
+            plugin.getSpawnMessageManager().sendMessage(player, "no-permission");
+            return true;
+        }
+        
+        plugin.getSpawnManager().setSpawn(player.getLocation());
+        plugin.getSpawnMessageManager().sendMessage(player, "spawn-set");
+        plugin.getSpawnSoundManager().playSpawnSet(player);
+        
+        return true;
+    }
+  }
